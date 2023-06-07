b022a127f701aed1fb987e0f0b9bb0693929f6f8
unknown <oakhmadjonov1@gmail.com>
2023-06-07 10:42:36 +0500

    To get start from scracht

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,17 @@
 import About from "../../pages/AboutSection/AboutHokimyat/AboutHokimyat";
+import Management from "../../pages/AboutSection/Management/Management";
 import Home from "../../pages/Home/Home";
-import Header from "../Header/Header";
+
 import "./App.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/management" element={<Management />} />
           <Route path="*" element={<Navigate to="/" replace={true} />} />
         </Routes>
       </BrowserRouter>
