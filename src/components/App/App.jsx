1c88325ddd44d26ebb1085f9e6ffc349a711481f
kingdev <kingdev5809@gmail.com>
2023-06-07 10:30:14 +0500

    first fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
 import About from "../../pages/AboutSection/AboutHokimyat/AboutHokimyat";
 import Home from "../../pages/Home/Home";
-import Header from "../Header/Header";
+import Navbar from "../Navbar/Navbar";
 import "./App.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header /> */}
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
